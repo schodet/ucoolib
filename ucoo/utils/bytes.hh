b17f8244eda5a3849d1af0a93744e345ae07d090
Nicolas Schodet <nico@ni.fr.eu.org>
2015-06-08 15:02:30 +0200

    ucoo/utils: add bytes buffer pack/unpack

--- ucoo/utils/bytes.hh
+++ ucoo/utils/bytes.hh
@@ -40,11 +40,53 @@ bytes_pack (uint8_t b3, uint8_t b2, uint8_t b1, uint8_t b0)
     return b3 << 24 | b2 << 16 | b1 << 8 | b0;
 }
 
+/// Pack a number of bytes to one word of any size, little endian.
+template<typename T>
+static inline T
+bytes_pack (const uint8_t *b)
+{
+    struct pack_struct
+    {
+        T value;
+    } __attribute__ ((packed));
+    return reinterpret_cast<const pack_struct *> (b)->value;
+}
+
+/// Pack a number of bytes to one word of any size, little endian, char
+/// version.
+template<typename T>
+static inline T
+bytes_pack (const char *b)
+{
+    return bytes_pack<T> (reinterpret_cast<const uint8_t *> (b));
+}
+
 /// Unpack one byte from one 32 bit word.
 static inline uint8_t
 bytes_unpack (uint32_t w, int n)
 {
     return (w >> (n * 8)) & 0xff;
+}
+
+/// Unpack a word of any size to a number of bytes, little endian.
+template<typename T>
+static inline void
+bytes_unpack (uint8_t *b, T value)
+{
+    struct pack_struct
+    {
+        T value;
+    } __attribute__ ((packed, aligned(1)));
+    reinterpret_cast<pack_struct *> (b)->value = value;
+}
+
+/// Unpack a word of any size to a number of bytes, little endian, char
+/// version
+template<typename T>
+static inline void
+bytes_unpack (char *b, T value)
+{
+    bytes_unpack (reinterpret_cast<uint8_t *> (b), value);
 }
 
 } // namespace ucoo
